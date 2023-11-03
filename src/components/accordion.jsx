cf17a624f7478997aa93b484503164c4f78b29a7
Rajarshi Sarkar <rajsarkar799@gmail.com>
2023-11-03 13:24:35 +0530

    Refine styles in Tailwind CSS for improved readability

--- src/components/accordion.jsx
+++ src/components/accordion.jsx
@@ -4,10 +4,13 @@ import data from "./data.js";
 import SingleQuestion from "./question.jsx";
 
 function Accordion() {
+  // State to manage the list of questions
   const [questions, setQuestions] = useState(data);
 
+  // State for managing the theme (light or dark)
   const [theme, setTheme] = useState(null);
 
+  // Effect to apply the selected theme
   useEffect(() => {
     if (theme === "dark") {
       document.documentElement.classList.add("dark");
@@ -16,22 +19,23 @@ function Accordion() {
     }
   }, [theme]);
 
+  // Function to toggle between light and dark themes
   const handleThemeSwitch = () => {
     setTheme(theme === "dark" ? "light" : "dark");
   };
 
   return (
-    <main className="overflow-x-scroll  min-h-screen bg-zinc-200 dark:bg-space dark:bg-cover">
-      <div className="flex justify-end">
+    <main className="overflow-x-scroll min-h-screen bg-zinc-200 dark:bg-space dark:bg-cover">
+      <div className="flex justify-end m-2">
         <button onClick={handleThemeSwitch}>
           <img
-            className="w-6"
-            src="https://static-00.iconduck.com/assets.00/dark-theme-icon-512x512-185rlszm.png"
+            className="w-8"
+            src="https://ps.w.org/wp-dark-mode/assets/icon-256x256.gif?rev=2872214"
             alt="dark mode icon"
           />
         </button>
       </div>
-      <div className=" w-10/12 max-w-4xl my-36 mx-auto rounded-md py-10 px-8 grid gap-y-5 gap-x-8 lg:flex lg:items-center bg-white border border-slate-800 dark:border-yellow-500  dark:bg-gradient-to-r from-slate-900 to-black">
+      <div className="w-10/12 max-w-4xl my-36 mx-auto rounded-md py-10 px-8 grid gap-y-5 gap-x-8 lg:flex lg:items-center bg-white border border-slate-800 dark:border-yellow-500  dark:bg-gradient-to-r from-slate-900 to-black shadow-xl shadow-black">
         <h3 className="text-center text-xl md:text-3xl font-semibold leading-10 tracking-wide italic dark:text-white font-mono">
           James Webb Space Telescope FAQs
         </h3>
--- src/components/question.jsx
+++ src/components/question.jsx
@@ -4,16 +4,17 @@ import { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 const Question = ({ title, info }) => {
+  // State to control whether the answer is shown or hidden
   const [showInfo, setShowInfo] = useState(false);
 
   return (
     <article className="py-4 px-6 mb-4 border text-white border-slate-400 rounded-md shadow-md shadow-slate-400 hover:shadow-slate-800 dark:shadow-slate-900 dark:hover:shadow-slate-500 bg-slate-800 dark:bg-yellow-500 dark:text-black">
       <header className="flex justify-between items-center">
-        <h4 className="text-base md:text-lg leading-6 tracking-wider font-mono font-semibold">
+        <h4 className="text-base md:text-lg leading-6 tracking-wider font-mono font-semibold mr-3">
           {title}
         </h4>
         <button
-          className="sm:w-6 sm:h-6 md:w-8 md:h-8 rounded-full bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-yellow-500 cursor-pointer flex justify-center items-center "
+          className="w-7 h-7 md:w-8 md:h-8 rounded-full p-2 bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-yellow-500 cursor-pointer flex justify-center items-center "
           onClick={() => setShowInfo(!showInfo)}
         >
           {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
